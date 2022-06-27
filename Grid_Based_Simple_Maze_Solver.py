ee41d957d1ebfcd83c0cc7607b8bcd6db12ad687
Kooreis <kooreis.official@gmail.com>
2022-06-27 05:00:00 -0500

    This update includes the import of the Queue module and the definition of the is_valid function. This function checks if a given sequence of moves is valid within the maze.

--- Grid_Based_Simple_Maze_Solver.py
+++ Grid_Based_Simple_Maze_Solver.py
@@ -1,5 +1,3 @@
-Sure, here is a simple Python script for a grid-based maze solver:
-
 ```python
 from queue import Queue
 
@@ -20,37 +18,4 @@ def is_valid(maze, moves):
         elif (maze[j][i] == 0):
             return False
     return True
-
-def find_end(maze, moves):
-    i = 0
-    j = 0
-    for move in moves:
-        if move == 'L':
-            i -= 1
-        elif move == 'R':
-            i += 1
-        elif move == 'U':
-            j -= 1
-        elif move == 'D':
-            j += 1
-    if maze[j][i] == 2:
-        print("Found: " + moves)
-        return True
-    return False
-
-maze = [[1, 0, 1, 1, 1, 1, 0, 1],
-        [1, 0, 0, 0, 0, 1, 0, 1],
-        [1, 1, 1, 0, 0, 1, 1, 1],
-        [1, 0, 1, 1, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1, 2]]
-
-nums = Queue()
-nums.put('')
-add = ''
-while not find_end(maze, add): 
-    add = nums.get()
-    for j in ['L', 'R', 'U', 'D']:
-        put = add + j
-        if is_valid(maze, put):
-            nums.put(put)
 ```

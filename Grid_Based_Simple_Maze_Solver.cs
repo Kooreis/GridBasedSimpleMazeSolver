29c28e261b08953fd2eec16695cbabf722d3b42f
Kooreis <kooreis.official@gmail.com>
2022-06-27 05:00:00 -0500

    Initializes the maze and path variables. The maze is a 2D array of characters representing the maze layout, and the path is a list of tuples representing the path through the maze.

--- Grid_Based_Simple_Maze_Solver.cs
+++ Grid_Based_Simple_Maze_Solver.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 using System.Collections.Generic;
 
@@ -14,49 +13,4 @@ public class Program
     };
 
     static List<Tuple<int, int>> path = new List<Tuple<int, int>>();
-
-    public static void Main()
-    {
-        if (FindPath(0, 0))
-        {
-            foreach (var point in path)
-            {
-                Console.WriteLine($"({point.Item1}, {point.Item2})");
-            }
-        }
-        else
-        {
-            Console.WriteLine("No path found!");
-        }
-    }
-
-    static bool FindPath(int x, int y)
-    {
-        if (x < 0 || y < 0 || x >= maze.GetLength(0) || y >= maze.GetLength(1))
-        {
-            return false;
-        }
-
-        if (maze[x, y] == 'E')
-        {
-            path.Add(new Tuple<int, int>(x, y));
-            return true;
-        }
-
-        if (maze[x, y] != ' ' && maze[x, y] != 'S')
-        {
-            return false;
-        }
-
-        maze[x, y] = '.';
-
-        if (FindPath(x - 1, y) || FindPath(x + 1, y) || FindPath(x, y - 1) || FindPath(x, y + 1))
-        {
-            path.Add(new Tuple<int, int>(x, y));
-            return true;
-        }
-
-        return false;
-    }
 }
-```
